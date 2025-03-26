2c02ff58eec9ba68876160fffad0786c32d080d2
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2025-03-26 22:34:13 +0000

    chore: add sample website using `vite`

---
+++ website/src/main.ts
@@ -0,0 +1,57 @@
+import { QRCode } from "../../dist";
+import { QRErrorCorrectLevel } from "../../dist/const";
+
+import "./style.css";
+
+// as SVG by ID
+new QRCode({
+  id: "qrcode-svg",
+  element: document.getElementById("qrcode-svg")!,
+  width: 256,
+  height: 256,
+  typeNumber: 4,
+  colorDark: "#000000",
+  colorLight: "#ffffff",
+  correctLevel: QRErrorCorrectLevel.H,
+  text: "https://example.com",
+  mode: "svg",
+});
+
+// as DOM by ID
+new QRCode({
+  id: "qrcode-dom",
+  width: 256,
+  height: 256,
+  typeNumber: 4,
+  colorDark: "#000000",
+  colorLight: "#ffffff",
+  correctLevel: QRErrorCorrectLevel.H,
+  text: "https://example.com",
+  mode: "dom",
+});
+
+// as SVG by Element
+new QRCode({
+  element: document.getElementById("qrcode-el-svg")!,
+  width: 256,
+  height: 256,
+  typeNumber: 4,
+  colorDark: "#000000",
+  colorLight: "#ffffff",
+  correctLevel: QRErrorCorrectLevel.Q,
+  text: "https://example.com",
+  mode: "svg",
+});
+
+// as DOM by ID
+new QRCode({
+  element: document.getElementById("qrcode-el-dom")!,
+  width: 256,
+  height: 256,
+  typeNumber: 4,
+  colorDark: "#000000",
+  colorLight: "#ffffff",
+  correctLevel: QRErrorCorrectLevel.L,
+  text: "https://example.com",
+  mode: "dom",
+});
